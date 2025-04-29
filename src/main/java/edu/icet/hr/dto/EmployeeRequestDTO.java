bbef450ed1df5336b930e162911c491ec5ce6de3
minindu-alwis <minindualwis@gmail.com>
2025-04-29 12:34:47 +0530

    Add @NotNull validation for department in EmployeeRequestDTO
    Ensure that the 'department' field is validated as mandatory during input. This change improves data integrity by preventing null values for the 'department' attribute. Also refactored imports for better readability and simplicity.

--- src/main/java/edu/icet/hr/dto/EmployeeRequestDTO.java
+++ src/main/java/edu/icet/hr/dto/EmployeeRequestDTO.java
@@ -1,10 +1,7 @@
 package edu.icet.hr.dto;
 
 import edu.icet.hr.util.Department;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,5 +20,6 @@ public class EmployeeRequestDTO {
     @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     private String email;
 
+    @NotNull(message = "Department is required")
     private Department department;
 }
