44eee6e605ec7b2d68e81cb893889de3c90189a5
minindu-alwis <minindualwis@gmail.com>
2025-04-29 10:00:39 +0530

    test

--- src/main/java/edu/icet/hr/Main.java
+++ src/main/java/edu/icet/hr/Main.java
@@ -4,6 +4,6 @@ package edu.icet.hr;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("working");
     }
 }
